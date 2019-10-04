069106025dd868fbde60c033f8c3cbccb9603c1a
Manuel Larios <mlarios12@wou.edu>
2019-10-03 20:14:57 -0700

    Working single character string compression

--- encode.py
+++ encode.py
@@ -1,2 +1,15 @@
 def encode(input_string):
+    index = 0
+    letter_count = 1
+
+    while index < len(input_string) - 1:
+        if(input_string[index] == input_string[index + 1]):
+            letter_count += 1 #increments letter_count
+            index += 1 #moves index to next element
+            continue
+        index += 1
+    
+    if(letter_count > 1):
+        return input_string[index] + str(letter_count)
+    
     return input_string
--- test_encode.py
+++ test_encode.py
@@ -4,7 +4,10 @@ def test_empty_string():
     assert encode("") == ""
 
 def test_single_character_string():
-    assert encode("a")
+    assert encode("a") == "a"
 
 def test_non_repeating_character_string():
-    assert encode("abcdefg")
+    assert encode("abcdefg") == "abcdefg"
+
+def test_single_character_string_compression():
+    assert encode("aaaa") == "a4"
