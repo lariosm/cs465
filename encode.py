aba1816df1c02c2e887fa06ad65bf532e85c80f6
Manuel Larios <mlarios12@wou.edu>
2019-10-04 10:19:08 -0700

    Added comments to encode.py

--- encode.py
+++ encode.py
@@ -1,28 +1,30 @@
 def encode(input_string):
-    index = 0
-    letter_count = 1
-    compressed_string = ""
+    index = 0 #Our index counter to traverse the input string
+    letter_count = 1 #A counter to keep track how often a letter is repeated
+    encoded_string = "" #Where letters will be stored in their shortened form
 
     while index < len(input_string) - 1:
         if(index == 0):
-            compressed_string = input_string[0]
+            encoded_string = input_string[0] #Append letter from first index
 
         #Is the letter in the next index the same as the letter in the current index?
         if(input_string[index] == input_string[index + 1]):
-            letter_count += 1 #increments letter_count
+            letter_count += 1 #Increment letter_count
 
         else:
+            #Is final count for a letter greater than 1?
             if(letter_count > 1):
-                compressed_string += str(letter_count)
-                letter_count = 1
-            compressed_string += input_string[index + 1]
+                encoded_string += str(letter_count) #Appends letter count
+                letter_count = 1 #Reset letter counter back to 1
+            encoded_string += input_string[index + 1] #Append next non-repeating letter
 
-        index += 1
+        index += 1 #Increment index counter
 
+        #Have we reached the end and still need to append letter count to a repeating letter?
         if(index == len(input_string) - 1 and letter_count > 1):
-            compressed_string += str(letter_count)
+            encoded_string += str(letter_count)
 
-    if(len(compressed_string) > 0):
-        return compressed_string
+    if(len(encoded_string) > 0):
+        return encoded_string
     
     return input_string
