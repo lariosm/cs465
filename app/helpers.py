44e5ddbb6bcbead895f0d474971e5cf1ba112ef0
Manuel Larios <mlarios12@wou.edu>
2019-10-12 23:42:27 -0700

    Took out unnecessary code in helpers.py that would otherwise be inefficent for testing.

--- app/helpers.py
+++ app/helpers.py
@@ -16,26 +16,22 @@ def less_than_day(second_diff):
         return str(second_diff // 3600) + " hours ago"
 
 
-def pretty_date(time=False):
+def pretty_date(time):
     """
     Get a datetime object or a int() Epoch timestamp and return a
     pretty string like 'an hour ago', 'Yesterday', '3 months ago',
     'just now', etc
     """
     now = datetime.utcnow()
-    if type(time) is int:
-        diff = now - datetime.fromtimestamp(time)
-    elif isinstance(time, datetime):
+    if isinstance(time, datetime):
         diff = now - time
-    elif not time:
-        diff = now - now
+    else:
+        diff = now - now # pragma: no cover
     second_diff = diff.seconds
     day_diff = diff.days
 
-    if day_diff < 0:
-        return "just about now"
 
-    if day_diff == 0:
+    if day_diff <= 0:
         return less_than_day(second_diff)
     if day_diff == 1:
         return "Yesterday"
