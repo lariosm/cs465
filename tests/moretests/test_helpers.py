bdf33564c1baabf827985a9a6eb945a91fa14c2e
Manuel Larios <mlarios12@wou.edu>
2019-10-12 20:53:59 -0700

    Working for checking if "x days ago" prints in helper.py

--- tests/moretests/test_helpers.py
+++ tests/moretests/test_helpers.py
@@ -4,3 +4,6 @@ from datetime import datetime, timedelta
 
 def test_yesterday():
     assert (pretty_date(datetime.utcnow() - timedelta(days=1))) == "Yesterday"
+
+def test_days_ago():
+    assert (pretty_date(datetime.utcnow() - timedelta(days=5))) == "5 days ago"
