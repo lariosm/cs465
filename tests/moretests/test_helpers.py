27a252036cf36915dd2675d4800b599c1862c564
Manuel Larios <mlarios12@wou.edu>
2019-10-12 21:16:27 -0700

    Working for checking if "x months ago" prints in helper.py

--- tests/moretests/test_helpers.py
+++ tests/moretests/test_helpers.py
@@ -7,3 +7,6 @@ def test_yesterday():
 
 def test_days_ago():
     assert (pretty_date(datetime.utcnow() - timedelta(days=5))) == "5 days ago"
+
+def test_months_ago():
+    assert (pretty_date(datetime.utcnow() - timedelta(weeks=11))) == "2 months ago"
