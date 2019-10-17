6a89b363a11acee601251b4e4eb4ac77573ffe7e
Manuel Larios <mlarios12@wou.edu>
2019-10-16 21:30:21 -0700

    Working GET: /api/activities/ that returns all activities in list

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, abort
+from datetime import datetime
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello world!"
+activity_log = [
+    {
+        'id': 0,
+        'user_id': 1,
+        'username': 'john',
+        'timestamp': datetime.utcnow(),
+        'details': "Important stuff here",
+    },
+    {
+        'id': 1,
+        'user_id': 2,
+        'username': 'yoko',
+        'timestamp': datetime.utcnow(),
+        'details': "Even more important",
+    },
+]
+
+@app.route('/api/activities', methods=["GET"])
+def activities():
+    return jsonify({'activities': activity_log})
