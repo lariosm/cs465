3aeb5f60d9c63ac7ff77eb2f639e9e20e22c3d50
Manuel Larios <mlarios12@wou.edu>
2019-10-28 23:51:48 -0700

    Got all methods in API mostly working.

--- app.py
+++ app.py
@@ -1,64 +1,67 @@
 from flask import Flask, jsonify, abort, request
 from datetime import datetime
-from mongoengine import connect, StringField, IntField, Document, DateTimeField
+from mongoengine import connect, StringField, IntField, Document, \
+    DateTimeField, queryset_manager
 import json
+from bson import ObjectId
 
 
 app = Flask(__name__)
 connect(db="act_log", host="localhost")
 
 
+# MongoDB document that describes schema for log entries
 class ActivityLog(Document):
     user_id = IntField(required=True)
     username = StringField(required=True, max_length=64)
     timestamp = DateTimeField(default=datetime.utcnow())
     details = StringField(required=True)
 
-
-activity_log = [
-    {
-        'id': 0,
-        'user_id': 1,
-        'username': 'john',
-        'timestamp': datetime.utcnow(),
-        'details': "Important stuff here",
-    },
-    {
-        'id': 1,
-        'user_id': 2,
-        'username': 'yoko',
-        'timestamp': datetime.utcnow(),
-        'details': "Even more important",
-    },
-]
+    @queryset_manager
+    def objects(self, queryset):
+        # By default, it orders queries by timestamp (most recent)
+        return queryset.order_by('-timestamp')
 
 
+# Returns all log entries
 @app.route('/api/activities/', methods=["GET"])
 def activities():
-    logs = ActivityLog.objects[:10]
-    return jsonify({'activities': json.loads(logs.to_json())})
-    # return jsonify({'activities': activity_log})
+    logs = ActivityLog.objects[:10]  # Returns first 10 entries
+    # Deserializes JSON document to python dict object
+    json_logs = json.loads(logs.to_json())
+    return jsonify({'activities': json_logs})
 
 
-@app.route('/api/activities/<id>', methods=["GET"])
-def activity(id):
-    # if id < 0 or id >= len(activity_log):
-    #    abort(404)
-    return jsonify(json.loads(ActivityLog.objects(_id=id).first().to_json()))
+# Returns a single log entry
+@app.route('/api/activities/<string:str_id>', methods=["GET"])
+def activity(str_id):
+    # Checks if input is valid (BSON) ObjectId object
+    if ObjectId.is_valid(str_id):
+        # Queries database by str_id and saves result to log_id
+        log_id = ActivityLog.objects(id=str_id).first()
+        if log_id is None:  # Does an entry with that str_id not exist?
+            abort(404)
+        # Deserializes JSON document to python dict object in readable format
+        log_id_json = json.loads(log_id.to_json())
+    return jsonify(log_id_json)
 
 
+# Creates and returns log entry
 @app.route('/api/activities/', methods=["POST"])
 def create_activity():
-    if not request.json:
+    if not request.json:  # Is POST request in JSON format?
         abort(400)
-    new_activity = request.get_json()
+    new_activity = request.get_json()  # Saves request to work with down below
     if ('user_id' not in new_activity or 'username' not in new_activity or
             'details' not in new_activity):
         abort(400)
+    # Creates and saves log entry
     new_log = ActivityLog(
         user_id=new_activity['user_id'],
         username=new_activity['username'],
         details=new_activity['details']
     ).save()
-    # new_activity['id'] = len(activity_log)
-    return jsonify(json.loads(new_log.to_json())), 201
+    # Deserializes JSON document to python dict object in readable format
+    new_log_json = json.loads(new_log.to_json())
+    # Returns entry in JSON format with status code 201
+    return jsonify(new_log_json), 201
