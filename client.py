55b74b79d6c5dd9f7a9db53c042dcc13c94a4cf3
Manuel Larios <mlarios12@wou.edu>
2019-12-03 10:52:47 -0800

    Created a console client for microservice

---
+++ client.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import sys
+
+
+def get_single_item(index):
+    get_url = "http://localhost:8080/api/posts"
+    try:
+        r = requests.get(get_url)
+        if r.status_code == 200:
+            data = json.loads(r.text)
+            print("Version: " + str(data[index - 1]["version"]))
+            if data[index - 1]["version"] == 2.0:
+                print("User IP: " + str(data[index - 1]["user_ip"]))
+    except requests.exceptions.RequestException:
+        print(f"Could not connect to the service at {get_url}")
+
+
+if __name__ == "__main__":
+    i = 0
+    while i < 6:
+        get_single_item(int(sys.argv[1]))
+        i = i + 1
